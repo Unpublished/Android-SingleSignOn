1c1b3fda3ef1ae823a4e7c6b8ac4d00490f2f277
Unpublished <unpublished@gmx.net>
2019-01-05 02:49:49 +0100

    Migrate to AndroidX

--- src/main/java/com/nextcloud/android/sso/AccountImporter.java
+++ src/main/java/com/nextcloud/android/sso/AccountImporter.java
@@ -29,7 +29,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.widget.Toast;
 
