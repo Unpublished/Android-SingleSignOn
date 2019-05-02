93b89db7ce13c13e14851d28efa981ad9dc138fd
Unpublished <unpublished@gmx.net>
2019-05-02 13:09:09 +0200

    Fix tests

---
+++ lib/src/main/java/okhttp3/NextcloudRequestBuilder.java
@@ -0,0 +1,17 @@
+package okhttp3;
+
+import java.util.List;
+
+public class NextcloudRequestBuilder {
+
+    public static String relativeUrl(HttpUrl apiEndpoint, String relativeUrl) {
+        HttpUrl resolvedUrl = apiEndpoint.resolve(relativeUrl);
+        List<String> pathSegments = resolvedUrl.pathSegments();
+        StringBuilder sb = new StringBuilder(resolvedUrl.encodedPath().length());
+        for (String seg : pathSegments) {
+            sb.append('/');
+            sb.append(seg);
+        }
+        return sb.toString();
+    }
+}
